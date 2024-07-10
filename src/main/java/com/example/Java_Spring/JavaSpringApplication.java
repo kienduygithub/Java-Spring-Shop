cc7f9bef973de75119e5bd0f6fb21012bd5c6039
KD TD <buikienduy2020@gmail.com>
2024-07-10 19:00:03 +0700

    #44

--- src/main/java/com/example/Java_Spring/JavaSpringApplication.java
+++ src/main/java/com/example/Java_Spring/JavaSpringApplication.java
@@ -2,20 +2,14 @@ package com.example.Java_Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(
+	exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+)
 public class JavaSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringApplication.class, args);
-		System.setProperty("spring.application.admin.enabled", "true");
-		// ApplicationContext hoidanIT = SpringApplication.run(JavaSpringApplication.class, args);
-		// for (String s : hoidanIT.getBeanDefinitionNames()) {
-		// 	System.out.println(s);
-		// }
-
 	}
 	
 }
